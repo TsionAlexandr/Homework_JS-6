8beee88e45a7015f14ed9f97e4c11187035b3300
TsionAlexandr <45336081+TsionAlexandr@users.noreply.github.com>
2019-01-15 23:46:55 +0200

    Update homework_dom-1.js

--- js/homework_dom-1.js
+++ js/homework_dom-1.js
@@ -1,32 +1,32 @@
 // // получить в консоль head
-// console.log (document.head);
+ console.log (document.head);
 
 // // получить в консоль body
-// console.log (document.body);
+ console.log (document.body);
 
 // // получить все дочерние элементы body и вывести их в консоль.
-// console.log (document.body.childNodes);
+console.log (document.body.childNodes);
 
 // // получить первый div и все его дочерние узлы
-// console.log (document.body.querySelector('div'));
-// console.log (document.body.querySelector('div').children);
+ console.log (document.body.querySelector('div'));
+ console.log (document.body.querySelector('div').children);
 
 // // вывести все дочерние узлы в консоль
 
-// if (document.body.querySelector('div').children) {
-//     const children1 = document.body.querySelector('div').children ;
+ if (document.body.querySelector('div').children) {
+     const children1 = document.body.querySelector('div').children ;
 
-//     for (var i = 0; i < children1.length; ++i) {
-//         console.log(children1[i])
-//     }
-// }
+     for (var i = 0; i < children1.length; ++i) {
+        console.log(children1[i])
+     }
+ }
 
 // // вывести в консоль все дочерние узлы, кроме первого и последнего
 
-// if (document.body.querySelector('div').children) {
-//     const children = document.body.querySelector('div').children ;
+ if (document.body.querySelector('div').children) {
+     const children = document.body.querySelector('div').children ;
 
-//     for (var i = 1; i < children.length - 1; ++i) {
-//         console.log(children[i])
-//     }
-// }
+     for (var i = 1; i < children.length - 1; ++i) {
+         console.log(children[i])
+     }
+ }
